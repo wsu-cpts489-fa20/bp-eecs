fa550b13181dec303f1fa02bdd1b0eb9b136a080
John Lee <john.k.lee@wsu.edu>
2020-11-13 00:48:13 -0800

    Changed app theme color to red to match WSU theme.

--- client/build/precache-manifest.d8224d73dd575da4add2800c70e9f69a.js
+++ client/build/precache-manifest.d8224d73dd575da4add2800c70e9f69a.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "807e1cb55beb7dd17dddbb5244c890a9",
+    "revision": "18b3c3bfff3c9887fa5ac29878f724e4",
     "url": "/index.html"
   },
   {
@@ -8,8 +8,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/css/2.d653346d.chunk.css"
   },
   {
-    "revision": "209987a794ab9b08c9a1",
-    "url": "/static/css/main.6a8397fb.chunk.css"
+    "revision": "894a11924ab42a83dc69",
+    "url": "/static/css/main.183daf3d.chunk.css"
   },
   {
     "revision": "99908da27a62c7151b0d",
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.928e17f6.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "209987a794ab9b08c9a1",
-    "url": "/static/js/main.8e64184a.chunk.js"
+    "revision": "894a11924ab42a83dc69",
+    "url": "/static/js/main.3ad7dcce.chunk.js"
   },
   {
     "revision": "19793d9fa96413d1c44d",

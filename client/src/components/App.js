5ffc82d671bad07d46fb5f65e2a61023cf14147c
John Lee <john.k.lee@wsu.edu>
2020-11-13 00:54:56 -0800

    Changed nav bar title to EECS Course Scheduler.

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -11,7 +11,7 @@ import CoursesPage from './CoursesPage.js';
 import AboutBox from './AboutBox.js';
 
 const modeTitle = {};
-modeTitle[AppMode.LOGIN] = "Welcome to SpeedScore";
+modeTitle[AppMode.LOGIN] = "EECS Course Scheduler";
 modeTitle[AppMode.FEED] = "Activity Feed";
 modeTitle[AppMode.ROUNDS] = "My Rounds";
 modeTitle[AppMode.ROUNDS_LOGROUND] = "Log New Round";

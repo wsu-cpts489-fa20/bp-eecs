1f000e5bfc17a64e12390c054c11f81c2324981a
David Brownell <davidabrownell@gmail.com>
2020-11-18 15:29:12 -0800

    Added an enumeration of majors

---
+++ client/src/Majors.js
@@ -0,0 +1,14 @@
+
+/*
+ * An enumeration of the majors which this website supports.
+ */
+const Majors = {
+    COMPUTER_SCIENCE: "ComputerScience",
+    COMPUTER_ENGINEERING: "ComputerEngineering",
+    ELECTRICAL_ENGINEERING: "ElectricalEngineering",
+    SOFTWARE_ENGINEERING: "SoftwareEngineering"
+};
+
+Object.freeze(Majors);
+
+export default Majors;

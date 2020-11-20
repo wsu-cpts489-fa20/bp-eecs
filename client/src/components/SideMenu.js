9bfa2eaad171cf7f31cae41d1f1c678f076b8e06
David Brownell <davidabrownell@gmail.com>
2020-11-20 01:10:30 -0800

    Refactor SideMenu.js to use React Router

--- client/src/components/SideMenu.js
+++ client/src/components/SideMenu.js
@@ -1,66 +1,63 @@
 import React from 'react';
-import AppMode from '../AppMode.js'
+import {Switch, Route} from "react-router-dom";
 
 class SideMenu extends React.Component {
-
-//renderModeItems -- Renders correct subset of mode menu items based on
-//current mode, which is stored in this.prop.mode. Uses switch statement to
-//determine mode.
-    renderModeMenuItems = () => {
-        switch (this.props.mode) {
-            case AppMode.FEED:
-                return (
-                    <div>
-                        <a className="sidemenu-item">
-                            <span className="fa fa-users"></span>&nbsp;Followed Users</a>
-                        <a className="sidemenu-item ">
-                            <span className="fa fa-search"></span>&nbsp;Search Feed</a>
-                    </div>
-                );
-                break;
-            case AppMode.ROUNDS:
-                return (
-                    <div>
-                        <a className="sidemenu-item">
-                            <span className="fa fa-plus"></span>&nbsp;Log New Round</a>
-                        <a className="sidemenu-item">
-                            <span className="fa fa-search"></span>&nbsp;Search Rounds</a>
-                    </div>
-                );
-                break;
-            case AppMode.COURSES:
-                return (
-                    <div>
-                        <a className="sidemenu-item">
-                            <span className="fa fa-plus"></span>&nbsp;Add a Course</a>
-                        <a className="sidemenu-item">
-                            <span className="fa fa-search"></span>&nbsp;Search Courses</a>
-                    </div>
-                );
-            default:
-                return null;
-        }
-    }
-
     render() {
         return (
-            <div className={"sidemenu " + (this.props.menuOpen ? "sidemenu-open" : "sidemenu-closed")}
+            <div className={`sidemenu ${this.props.menuOpen ? "sidemenu-open" : "sidemenu-closed"}`}
                  onClick={this.props.toggleMenuOpen}>
-                {/* SIDE MENU TITLE */}
                 <div className="sidemenu-title">
                     <img src={this.props.profilePicURL} height='60' width='60'/>
                     <span id="userID" className="sidemenu-userID">&nbsp;{this.props.displayName}</span>
                 </div>
-                {/* MENU CONTENT */}
-                {this.renderModeMenuItems()}
-                {/* The following menu items are present regardless of mode */}
-                {this.props.localAccount ?
-                    <a id="accountBtn" className="sidemenu-item" onClick={this.props.editAccount}>
-                        <span className="fa fa-user"></span>&nbsp;Account</a> : null}
+
+                <Switch>
+                    <Route path="/feed">
+                        <div>
+                            <a className="sidemenu-item">
+                                <span className="fa fa-users"/>
+                                &nbsp;Followed Users
+                            </a>
+                            <a className="sidemenu-item ">
+                                <span className="fa fa-search"/>
+                                &nbsp;Search Feed
+                            </a>
+                        </div>
+                    </Route>
+                    <Route path="/rounds">
+                        <div>
+                            <a className="sidemenu-item">
+                                <span className="fa fa-plus"/>
+                                &nbsp;Log New Round
+                            </a>
+                            <a className="sidemenu-item">
+                                <span className="fa fa-search"/>
+                                &nbsp;Search Rounds
+                            </a>
+                        </div>
+                    </Route>
+                    <Route path="/courses">
+                        <div>
+                            <a className="sidemenu-item">
+                                <span className="fa fa-plus"/>
+                                &nbsp;Add a Course
+                            </a>
+                            <a className="sidemenu-item">
+                                <span className="fa fa-search"/>
+                                &nbsp;Search Courses
+                            </a>
+                        </div>
+                    </Route>
+                </Switch>
+                {this.props.localAccount
+                    ? <a id="accountBtn" className="sidemenu-item" onClick={this.props.editAccount}>
+                        <span className="fa fa-user"/>&nbsp;Account
+                    </a>
+                    : null}
                 <a id="aboutBtn" className="sidemenu-item" onClick={this.props.showAbout}>
-                    <span className="fa fa-info-circle"></span>&nbsp;About</a>
+                    <span className="fa fa-info-circle"/>&nbsp;About</a>
                 <a id="logOutBtn" className="sidemenu-item" onClick={this.props.logOut}>
-                    <span className="fa fa-sign-out-alt"></span>&nbsp;Log Out</a>
+                    <span className="fa fa-sign-out-alt"/>&nbsp;Log Out</a>
             </div>
         );
     }

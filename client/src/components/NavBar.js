5e21afe92867cca03549fc7f62347fe34fc7f32e
David Brownell <davidabrownell@gmail.com>
2020-11-19 23:09:25 -0800

    Reformat NavBar.js

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -3,42 +3,42 @@ import AppMode from '../AppMode';
 
 class NavBar extends React.Component {
 
-  getMenuBtnIcon = () => {
-      if (this.props.mode === AppMode.ROUNDS_LOGROUND ||
-          this.props.mode === AppMode.ROUNDS_EDITROUND)
-          return "fa fa-arrow-left";
-      if (this.props.menuOpen)
-        return "fa fa-times";
-      return "fa fa-bars";
-  }
-
-  handleMenuBtnClick = () => {
-    if (this.props.mode === AppMode.ROUNDS_LOGROUND ||
-        this.props.mode === AppMode.ROUNDS_EDITROUND) {
-      this.props.changeMode(AppMode.ROUNDS);
-    } else if (this.props.mode != AppMode.LOGIN) {
-      this.props.toggleMenuOpen();
+    getMenuBtnIcon = () => {
+        if (this.props.mode === AppMode.ROUNDS_LOGROUND ||
+            this.props.mode === AppMode.ROUNDS_EDITROUND)
+            return "fa fa-arrow-left";
+        if (this.props.menuOpen)
+            return "fa fa-times";
+        return "fa fa-bars";
     }
-  }
 
-    
-  render() {
-    return (
-    <div className="navbar">  
+    handleMenuBtnClick = () => {
+        if (this.props.mode === AppMode.ROUNDS_LOGROUND ||
+            this.props.mode === AppMode.ROUNDS_EDITROUND) {
+            this.props.changeMode(AppMode.ROUNDS);
+        } else if (this.props.mode != AppMode.LOGIN) {
+            this.props.toggleMenuOpen();
+        }
+    }
+
+
+    render() {
+        return (
+            <div className="navbar">
     <span className="navbar-items">
       <button className="sidemenu-btn" onClick={this.handleMenuBtnClick}>
         <span id="menuBtnIcon" className={"sidemenu-btn-icon " + this.getMenuBtnIcon()}>
         </span>
       </button>
       <img src={require('../images/WSU-logo.png')} alt="WSU Logo" height="38px"
-      width="38px" />
+           width="38px"/>
       <span className="navbar-title">
         &nbsp;{this.props.title}
       </span>
     </span>
-  </div>
-); 
-}
+            </div>
+        );
+    }
 }
 
 export default NavBar;

42d2aa77bf9a15e340877e869f5dbd19f09d75d2
David Brownell <davidabrownell@gmail.com>
2020-11-19 23:17:37 -0800

    Reformat Navbar.js

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -25,17 +25,16 @@ class NavBar extends React.Component {
     render() {
         return (
             <div className="navbar">
-    <span className="navbar-items">
-      <button className="sidemenu-btn" onClick={this.handleMenuBtnClick}>
-        <span id="menuBtnIcon" className={"sidemenu-btn-icon " + this.getMenuBtnIcon()}>
-        </span>
-      </button>
-      <img src={require('../images/WSU-logo.png')} alt="WSU Logo" height="38px"
-           width="38px"/>
-      <span className="navbar-title">
-        &nbsp;{this.props.title}
-      </span>
-    </span>
+                <span className="navbar-items">
+                    <Switch>
+                        <button className="sidemenu-btn" onClick={this.handleMenuBtnClick}>
+                            <span id="menuBtnIcon" className={"sidemenu-btn-icon " + this.getMenuBtnIcon()}/>
+                        </button>
+                    </Switch>
+                    <img src={require('../images/WSU-logo.png')} alt="WSU Logo" height="38px"
+                         width="38px"/>
+                    <span className="navbar-title">&nbsp;{this.props.title}</span>
+                </span>
             </div>
         );
     }

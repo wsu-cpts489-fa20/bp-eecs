bd3619d46b384366c55c96313d3f8b014e2f2512
David Brownell <davidabrownell@gmail.com>
2020-11-20 00:48:15 -0800

    Reformat CoursesPage.js

--- client/src/components/CoursesPage.js
+++ client/src/components/CoursesPage.js
@@ -4,17 +4,17 @@ class CoursesPage extends React.Component {
 
     render() {
         return (
-        <div className="padded-page">
-            <center>
-            <h1 >Courses</h1>
-            <h2>This page is under construction.</h2>
-            <img src={require('../images/WSU-logo.png')}
-             height="200" width="200"/>
-            <p style={{fontStyle: "italic"}}>Version CptS 489 React Demo</p>
-            </center>
-        </div>
+            <div className="padded-page">
+                <center>
+                    <h1>Courses</h1>
+                    <h2>This page is under construction.</h2>
+                    <img src={require('../images/WSU-logo.png')}
+                         height="200" width="200"/>
+                    <p style={{fontStyle: "italic"}}>Version CptS 489 React Demo</p>
+                </center>
+            </div>
         );
-    }   
+    }
 }
 
 export default CoursesPage;

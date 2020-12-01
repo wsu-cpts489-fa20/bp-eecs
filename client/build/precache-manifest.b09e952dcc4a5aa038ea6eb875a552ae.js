085a86c3009f4adfe9ee49ed9cc06efc73c20b61
Nick Singer <nicholas.singer25@gmail.com>
2020-11-30 22:08:45 -0800

    Added AWS Elastic Beanstalk files

---
+++ client/build/precache-manifest.b09e952dcc4a5aa038ea6eb875a552ae.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d379bf6d72fadbbc73fc5e09378a35f1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c40cafe436525beb1a31",
+    "url": "/static/css/2.d653346d.chunk.css"
+  },
+  {
+    "revision": "e2f78eb728a70f7bbbfa",
+    "url": "/static/css/main.1eac6578.chunk.css"
+  },
+  {
+    "revision": "c40cafe436525beb1a31",
+    "url": "/static/js/2.7e295ccf.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.7e295ccf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e2f78eb728a70f7bbbfa",
+    "url": "/static/js/main.65b0f898.chunk.js"
+  },
+  {
+    "revision": "19793d9fa96413d1c44d",
+    "url": "/static/js/runtime-main.0d575dc6.js"
+  },
+  {
+    "revision": "d56d53717be404f1844d655f8cbd0c94",
+    "url": "/static/media/WSU-background.d56d5371.jpg"
+  },
+  {
+    "revision": "d4057e66446217a85a54d5bc3ee450bc",
+    "url": "/static/media/WSU-logo.d4057e66.png"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);

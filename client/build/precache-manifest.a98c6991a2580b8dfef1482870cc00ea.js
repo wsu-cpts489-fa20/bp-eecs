c812e2df13c60979e5baab25d2a59b3cce37358e
Nick Singer <nicholas.singer25@gmail.com>
2020-12-01 17:37:03 -0800

    Edited proc file to npm run serve

---
+++ client/build/precache-manifest.a98c6991a2580b8dfef1482870cc00ea.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab559a7a5cd98adc08a3bd24ec5d2e49",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6b7fc3743015d1e47737",
+    "url": "/static/css/2.00b6ad22.chunk.css"
+  },
+  {
+    "revision": "74055965b0225416173c",
+    "url": "/static/css/main.52acaf3d.chunk.css"
+  },
+  {
+    "revision": "6b7fc3743015d1e47737",
+    "url": "/static/js/2.4b8ed3d8.chunk.js"
+  },
+  {
+    "revision": "0d08bb566150086a6317785a01bb3007",
+    "url": "/static/js/2.4b8ed3d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "74055965b0225416173c",
+    "url": "/static/js/main.034a10d4.chunk.js"
+  },
+  {
+    "revision": "19793d9fa96413d1c44d",
+    "url": "/static/js/runtime-main.0d575dc6.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);

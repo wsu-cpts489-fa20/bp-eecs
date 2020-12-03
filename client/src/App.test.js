810d4f43f63ac89637fdadf3b75f8f54065e98cb
David Brownell <davidabrownell@gmail.com>
2020-12-03 00:56:54 -0800

    Add a test to ensure that the logout feature works

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import {render} from '@testing-library/react';
 import App from './components/App';
-import {HashRouter} from "react-router-dom";
+import {MemoryRouter, Router} from "react-router-dom";
+import {createMemoryHistory} from 'history';
 
 test('renders app', () => {
-    const {getAllByText} = render(<HashRouter><App/></HashRouter>);
+    const {getAllByText} = render(<MemoryRouter><App/></MemoryRouter>);
     const appTitle = getAllByText(/EECS Course Scheduler/i);
     for (const htmlElement of appTitle) {
         expect(htmlElement).toBeInTheDocument()
+    }
+});
+
+test('logged-out redirect works', () => {
+    for (const path of ["/", "/eecs", "/does-not-exist"]) {
+        const history = createMemoryHistory();
+        history.push(path);
+        render(<Router history={history}><App/></Router>);
+        expect(history.location.pathname).toBe("/login");
     }
 });

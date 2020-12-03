310ed6aac2b9dfdc031e3e3595c4b9d18cd559f1
David Brownell <davidabrownell@gmail.com>
2020-12-02 23:42:12 -0800

    Fix basic client-side test

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,9 +1,12 @@
 import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import {render} from '@testing-library/react';
+import App from './components/App';
+import {HashRouter} from "react-router-dom";
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders app', () => {
+    const {getAllByText} = render(<HashRouter><App/></HashRouter>);
+    const appTitle = getAllByText(/EECS Course Scheduler/i);
+    for (const htmlElement of appTitle) {
+        expect(htmlElement).toBeInTheDocument()
+    }
 });

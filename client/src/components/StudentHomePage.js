7272c65fbf485251c78e15db5a96a7a03a3c963f
John Lee <john.k.lee@wsu.edu>
2020-12-10 13:02:42 -0800

    Begin implementation of the student table. Currentnly unable to populate table with items.

--- client/src/components/StudentHomePage.js
+++ client/src/components/StudentHomePage.js
@@ -1,70 +1,121 @@
-import React from 'react';
-import {Link, Switch, Route, useHistory} from "react-router-dom";
+import React from "react";
+import { Link, Switch, Route, useHistory } from "react-router-dom";
+//import StudentTable from "./StudentTable.js";
+import { withRouter } from "react-router";
 
 class StudentHomePage extends React.Component {
-    constructor(props){
-        super(props);
-        this.state = {major: "Computer Science"}
-    }
+  constructor(props) {
+    super(props);
+    this.state = { major: "Computer Science" };
+  }
 
-    
-    handleChange = async (event) => {
-        this.setState({major: event.target.value});
-        const url = '/rounds/' + this.props.userObj.id;
-        const res = await fetch(url, {
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json'
-            },
-            method: 'POST',
-            body: JSON.stringify(this.state.major)
-        });
-        const msg = await res.text();
-        if (res.status !== 200) {
-            this.setState({errorMsg: msg});
-        } else {
-            this.setState({errorMsg: ""});
-            await this.props.refreshOnUpdate();
-        }
-        this.props.history.goBack()
+  handleChange = async (event) => {
+    this.setState({ major: event.target.value });
+    const url = "/rounds/" + this.props.userObj.id;
+    const res = await fetch(url, {
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      method: "POST",
+      body: JSON.stringify(this.state.major),
+    });
+    const msg = await res.text();
+    if (res.status !== 200) {
+      this.setState({ errorMsg: msg });
+    } else {
+      this.setState({ errorMsg: "" });
+      await this.props.refreshOnUpdate();
     }
-    render() {
-        return (
-        <div id = "HomePage"className="padded-page">
-            <center>
-            <h1 >Welcome </h1>
-            <h2> Student Home Page</h2>
-            </center>
-            <label style={{textAlign:"left"}}>Select your major:
-                <select name="major" value={this.state.major} 
-                className="form-control form-center" onChange={this.handleChange}>
-                    <option value="Computer Science">Computer Science</option>
-                    <option value="Computer Engineering">Computer Engineering</option>
-                    <option value="Electrical Engineering">Electrical Engineering</option>
-                    <option value="Software Engineering">Software Engineering</option>
-                </select> 
-            </label>
-            <center>
-                <h1>Current Courses</h1>
-            </center>
-            <p></p>
-            <table className="table table-hover">
-                <thead className="thead-light">
-                <tr>
-                    <th>Course</th>
-                    <th>Course Name</th>
-                    <th>Course Days</th>
-                </tr>
-                </thead>
-            </table>
-            <p></p>
-            <h3 style={{textAlign:"left"}}>Class Dependeny Graph</h3>
-            <p></p>
-            <h4>Future Classes List</h4>
-        </div> 
+    this.props.history.goBack();
+  };
+
+  //renderTable -- render an HTML table displaying the rounds logged
+  //by the current user and providing buttons to view/edit and delete each round.
+  //render each table based on tableMode prop which contains "/cpte"
+  renderTable = () => {
+    const table = [];
+    //const tableMode = this.props.tableMode;
+    //const courseTag = tableMode.slice(1);
+    const currentUrl = this.props.history.location.pathname;
+    for (const round of this.props.rounds) {
+      //if (round[courseTag]) {
+        table.push(
+          <tr key={round._id}>
+            <td>{round.courseId}</td>
+            <td>{round.courseName}</td>
+            <td>{round.description}</td>
+            <td>{round.prerequisites}</td>
+          </tr>
         );
-    }   
-}
+      }
+   // }
+    return table;
+  };
 
+  //render -- Conditionally render the Rounds mode page as either the rounds
+  //table, the rounds form set to obtain a new round, or the rounds form set
+  //to edit an existing round.
+  render() {
+    return (
+      <div className="padded-page">
+        <div className="studentHeader">
+          <div className="studentDropdown">
+            <label style={{ textAlign: "left" }}>
+              Select your major:
+              <select
+                name="major"
+                value={this.state.major}
+                className="form-control form-center"
+                onChange={this.handleChange}
+              >
+                <option value="Computer Science">Computer Science</option>
+                <option value="Computer Engineering">
+                  Computer Engineering
+                </option>
+                <option value="Electrical Engineering">
+                  Electrical Engineering
+                </option>
+                <option value="Software Engineering">
+                  Software Engineering
+                </option>
+              </select>
+            </label>
+          </div>
+
+          <div className="studentTitle">
+            <h1>Courses - Spring 2021</h1>
+          </div>
+
+          <table className="table table-hover">
+            <thead className="thead-light">
+              <tr>
+                <th>Course</th>
+                <th>Title</th>
+                <th>Credits</th>
+                <th>Days & Time</th>
+                <th>Instructor</th>
+                <th>Enrolled</th>
+              </tr>
+            </thead>
+
+            {/* <tbody>
+              {Object.keys(this.props.rounds).length === 0 ? (
+                <tr>
+                  <td colSpan="5" style={{ fontStyle: "italic" }}>
+                    No courses logged
+                  </td>
+                </tr>
+              ) : (
+                this.renderTable()
+              )}
+            </tbody> */}
+
+          </table>
+        </div>
+      </div>
+    );
+  }
+}
 
 export default StudentHomePage;
--- client/src/components/StudentView.js
+++ client/src/components/StudentView.js
@@ -1,9 +1,18 @@
 import React from "react";
 import StudentHomePage from "./StudentHomePage.js"
+import Majors from "../Majors";
+import Rounds from "./Rounds";
+import {Route, Switch} from "react-router-dom";
+
 class StudentView extends React.Component {
     render() {
         return (
-            <StudentHomePage  userObj={this.props.userObj}/>
+            <StudentHomePage  
+            userObj={this.props.userObj}
+            refreshOnUpdate={this.props.refreshOnUpdate}
+            menuOpen={this.props.menuOpen}
+            
+            />
         );
     }
 }
